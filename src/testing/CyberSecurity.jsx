5b8091caffcfebc102119a58bc55ec3609392c24
zohaibyounas <zohaiby737@gmail.com>
2026-01-14 19:19:19 +0500

    updated

---
+++ src/testing/CyberSecurity.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CyberSecurity = () => {
+  return <div>coming soon</div>;
+};
+
+export default CyberSecurity;
--- src/testing/EmbeddedSystems.jsx
+++ src/testing/EmbeddedSystems.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Bms = () => {
+const EmbeddedSystems = () => {
   return <div>Coming soon</div>;
 };
 
-export default Bms;
+export default EmbeddedSystems;
